c2a5f18e1ab98001faa3081061114d24ca052c03
jennifernakano123 <59908819+jennifernakano123@users.noreply.github.com>
2020-01-14 20:42:48 -1000

    Create Robot.java

---
+++ src/main/java/frc/robot/main/Robot.java
@@ -0,0 +1,53 @@
+package frc.robot.main;
+
+import edu.wpi.first.wpilibj.TimedRobot;
+import edu.wpi.first.wpilibj.XboxController;
+import edu.wpi.first.wpilibj.GenericHID.Hand;
+import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+
+import frc.robot.subsystems.DriveTrain;
+
+/* Robot Code */
+public class Robot extends TimedRobot {
+  private static final String kDefaultAuto = "Default";
+  private static final String kCustomAuto = "My Auto";
+  private String m_autoSelected;
+  private final SendableChooser<String> m_chooser = new SendableChooser<>();
+  DriveTrain driveTrain = new DriveTrain();
+  XboxController joystk = new XboxController(0);
+
+  /* Robot Init */
+  @Override
+  public void robotInit() {
+    m_chooser.setDefaultOption("Default Auto", kDefaultAuto);
+    m_chooser.addOption("My Auto", kCustomAuto);
+    SmartDashboard.putData("Auto choices", m_chooser);
+  }
+
+  /* Auto Init */
+  @Override
+  public void autonomousInit() {
+    m_autoSelected = m_chooser.getSelected();
+    // m_autoSelected = SmartDashboard.getString("Auto Selector", kDefaultAuto);
+    System.out.println("Auto selected: " + m_autoSelected);
+  }
+  /* Auto Code */
+  @Override
+  public void autonomousPeriodic() {
+    switch (m_autoSelected) {
+      case kCustomAuto:
+        // Put custom auto code here
+        break;
+      case kDefaultAuto:
+      default:
+        // Put default auto code here
+        break;
+    }
+  }
+  /* Teleop Code */
+  @Override
+  public void teleopPeriodic() {
+    driveTrain.teleop(joystk.getY(Hand.kLeft), joystk.getY(Hand.kRight));
+  }
+}
