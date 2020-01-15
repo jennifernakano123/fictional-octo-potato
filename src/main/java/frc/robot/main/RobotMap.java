035d65795324b85b8ab40ff22f7ecfe811f54ca6
jennifernakano123 <59908819+jennifernakano123@users.noreply.github.com>
2020-01-14 20:43:20 -1000

    Create RobotMap.java

---
+++ src/main/java/frc/robot/main/RobotMap.java
@@ -0,0 +1,32 @@
+/*----------------------------------------------------------------------------*/
+/* Copyright (c) 2018-2019 FIRST. All Rights Reserved.                        */
+/* Open Source Software - may be modified and shared by FRC teams. The code   */
+/* must be accompanied by the FIRST BSD license file in the root directory of */
+/* the project.                                                               */
+/*----------------------------------------------------------------------------*/
+
+package frc.robot.main;
+
+/**
+ * Add your docs here.
+ */
+public class RobotMap {
+    public static int 
+        drive_right_M = 12,
+        drive_left_M = 10,
+        drive_right_S = 20,
+        drive_left_S = 11;
+    public static int
+        arm_rotator = 6,
+        arm_intake = 7;
+    public static int
+        shooter_M = 8;
+    public static int
+        hopper_adjust = 9,
+        hopper_feeder = 10;
+    public static int
+        hang_lift = 14,
+        hang_adjust = 15;
+    public static int
+        joystk;
+}
